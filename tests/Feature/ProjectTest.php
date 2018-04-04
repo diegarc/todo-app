0b22ea520cf85557505298845b92442847d03418
Diego M. Garcia M <diegarc@gmail.com>
2018-04-03 22:25:43 -0500

    Users table seeder Project tests

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,19 +2,43 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProjectTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
-     *
-     * @return void
+     * Project create.
      */
     public function testCreate()
     {
-        $this->assertTrue(true);
+        $user = factory(User::class)->create();
+        $data = [
+            'name' => 'Project name',
+            'description' => 'Project description'
+        ];
+
+        $this->actingAs($user)->post('/projects', $data);
+
+        $this->assertDatabaseHas('projects', $data);
+    }
+
+    /**
+     * Project create validations.
+     */
+    public function testCreateValidation()
+    {
+        $user = factory(User::class)->create();
+        $data = ['name' => ''];
+
+        $response = $this->actingAs($user)->post('/projects', $data);
+
+        $response->assertSessionHasErrors(['name']);
+        $response->assertSessionMissing(['description']);
+        $this->assertDatabaseMissing('projects', $data);
     }
 }
